e6da2895ea88b24e42f677b635ba560e13f04bbd
Julian <20096457+Jric0@users.noreply.github.com>
2019-11-12 15:59:01 -0800

    [1.1.1] GitLab Support & Fixing Announcements

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const pj = require("path").join
 const {spawn} = require("child_process")
 const fs = require("fs")
 
-function announce(message) {
-	powercord.pluginManager.get("pc-announcements").sendNotice({
-		id: "install-from-link",
-		message,
-		alwaysDisplay: true
+function announce(message, color) {
+	powercord.pluginManager.get("pc-notices").sendAnnouncement("install-from-link", {
+		message: message,
+		color: color
 	})
 }
 
@@ -28,7 +27,7 @@ module.exports = class InstallFromLink extends Plugin {
 			const {target} = this.props;
 			if (target.tagName.toLowerCase() === "a") {
 				const href = target.href
-				let match = href.match(/^https?:\/\/(www.)?github.com\/[\w-]+\/[\w-]+/)
+				let match = href.match(/^https?:\/\/(www.)?(github||gitlab).com\/[\w-]+\/[\w-]+/)
 				if (match) {
 					const link = match[0]
 					const repoName = link.match(/[\w-]+$/)[0]
@@ -58,7 +57,7 @@ module.exports = class InstallFromLink extends Plugin {
 										windowsHide: true
 									})
 								} catch (e) { // spawn failed
-									return announce("Couldn't start the installer. Do you have git installed?")
+									return announce("Couldn't start the installer. Do you have git installed?", "red")
 								}
 								c.stdout.on("data", data => console.log(data.toString()))
 								c.stderr.on("data", data => {
@@ -72,21 +71,21 @@ module.exports = class InstallFromLink extends Plugin {
 										try {
 											files = fs.readdirSync(pj(pluginsDir, repoName))
 										} catch (e) { // readdirSync failed
-											return announce(`Cloning apparently succeeded, but the resulting installation folder was nowhere to be found. (Looking for ${pj(pluginsDir, repoName)})`) // just do this for now I guess
+											return announce(`Cloning apparently succeeded, but the resulting installation folder was nowhere to be found. (Looking for ${pj(pluginsDir, repoName)})`, "yellow") // just do this for now I guess
 										}
 										if (files.includes("manifest.json")) {
 											await powercord.pluginManager.remount(repoName)
 											if (powercord.pluginManager.plugins.has(repoName)) {
-												announce("The plugin was installed and loaded successfully!")
+												announce("The plugin was installed and loaded successfully!", "green")
 											} else { // remount mysteriously failed
-												announce("The plugin was installed, but you'll need to press Ctrl-R to reload Discord before you can use it.")
+												announce("The plugin was installed, but you'll need to press Ctrl-R to reload Discord before you can use it.", "green")
 											}
 										} else { // no manifest
-											announce("That repo is not a Powercord plugin. It's been cloned anyway, but don't expect it to do anything.")
+											announce("That repo is not a Powercord plugin. It's been cloned anyway, but don't expect it to do anything.", "orange")
 										}
 									} else { // non-zero exit code
-										if (status) announce(status)
-										else announce(`The command mysteriously exited with a non-zero status code (${code}). Do y`)
+										if (status) announce(status, "red")
+										else announce(`The command mysteriously exited with a non-zero status code (${code}). Do y`, "red")
 									}
 								})
 							}
